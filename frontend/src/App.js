2f898d41d5c3c9651545da8a3ac09c35fe372c03
youssefamr367 <103262351+youssefamr367@users.noreply.github.com>
2025-07-19 20:02:11 +0300

    yyy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar            from "./components/Navbar";
-import ProductList       from "./pages/ProductList";
-import OrderList         from "./pages/OrderList";
-import SupplierList      from "./pages/SupplierList";
-import OrderDashboard    from "./pages/OrderDashboard";
-import LookupManager     from "./pages/LookupManager";
-import React from "react";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => (
-    <Router>
-        <Navbar />
-        <div className="container">
-            <Routes>
-                <Route path="/"             element={<OrderDashboard />} />
-                <Route path="/products"     element={<ProductList />} />
-                <Route path="/orders"       element={<OrderList />} />
-                <Route path="/suppliers"    element={<SupplierList />} />
-                <Route path="/Materials"    element={<LookupManager />} />
-            </Routes>
-        </div>
-    </Router>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+import reportWebVitals from './reportWebVitals';
 
-if (process.env.NODE_ENV === "production") disableReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,3 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
